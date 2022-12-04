1d18bf4b580f13f6742f7c43b8b02910c9f1b4eb
Cédric PIERSON <cdric.pierson@gmail.com>
2022-12-04 19:21:16 +0100

    update images links

--- pages/crypto.tsx
+++ pages/crypto.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import cryptoImg from "assets/projects/crypto.jpg";
+import cryptoImg from "/assets/projects/crypto.jpg";
 
 import { RiRadioButtonFill } from "react-icons/ri";
 import Link from "next/link";
